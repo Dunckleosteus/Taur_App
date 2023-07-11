458982c7ffb98d86dd6b252bace63aa7c7151861
George Husband <ghus266@gmail.com>
2023-07-11 16:26:25 +0200

    playing with css stylesheet

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,22 @@
 use yew::prelude::*;
 pub enum Msg{
-    Msg1,
+    ChangePage,
 }
-
+pub enum Pages{
+    Main,
+    Env,
+}
 pub struct App {
-    name:String
+   page: Pages
 }
 impl Component for App {
     type Message = Msg; // set to enum
     type Properties = ();
+
     fn create (_ctx: &Context<Self>)->Self{
-        Self {name: "Hello".into()}
+        Self {page: Pages::Main}
     }
+
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message)->bool{
     // handles messages set to the component
         match msg{
@@ -20,12 +25,28 @@ impl Component for App {
             }
         }
     }
+
     fn view(&self, ctx: &Context<Self>) -> Html {
         // describes how a component should be rendered
-        let link = ctx.link();{
-            html!{
-                
+        let link = ctx.link();
+        match self.page{
+            Pages::Main => {
+                html!{
+                    <div class={"dropdown"}>
+                    <button class={"dropbtn"}>{"Dropdown"}</button>
+                        <div class={"dropdown-content"}>
+                            <a onclick=link.callback(||)>{"Link 1"}</a>
+                            <a href="#">{"Link 2"}</a>
+                            <a href="#">{"Link 3"}</a>
+                        </div>
+                    </div>
                 }
-            }
+            },
+            Pages::Env => {
+                html!{
+                    <h1> {"Environment Page"} </h1>
+                }
+            },
+        }
     }
 }
